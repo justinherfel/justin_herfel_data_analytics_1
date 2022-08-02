cf7e5fa1991b4c0734ad22c3a7f81431e2591678
Justin Herfel <justinherfel@yahoo.com>
2022-08-02 19:59:54 -0400

    first attempt to add code that reads strings as ints in 2040 column of numbers - doesn't work yet

--- justin_herfel_data_analytics_1.py
+++ justin_herfel_data_analytics_1.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
 import os
 
 df = pd.read_csv(os.path.join(
   os.path.dirname(__file__), 'assets', 'NationalProjections_ProjectedTotalPopulation_2020_2040_Updated12_2018.csv'))
 
+df['2040'] = df['2040'].replace(",", "")
 population = df.nlargest(5, '2040')
 print(population)
+
 
 x_axis = population["State"]
 y_axis = population["2040"]
