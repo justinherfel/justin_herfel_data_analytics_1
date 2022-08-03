b5ecd31b3a45256a48c74a028236d370c57dc1f9
Justin Herfel <justinherfel@yahoo.com>
2022-08-02 20:35:12 -0400

    changed data to 2020 top 5 states in population

--- justin_herfel_data_analytics_1.py
+++ justin_herfel_data_analytics_1.py
@@ -5,21 +5,21 @@ import os
 df = pd.read_csv(os.path.join(
   os.path.dirname(__file__), 'assets', 'NationalProjections_ProjectedTotalPopulation_2020_2040_Updated12_2018.csv'))
 
-df['2040'] = df['2040'].str.replace(",", "")
-df['2040'] = df['2040'].astype('int')
-print(df.dtypes)
-population = df.nlargest(5, '2040')
+df['2020'] = df['2020'].str.replace(",", "")
+df['2020'] = df['2020'].astype('int')
+# print(df.dtypes)
+population = df.nlargest(5, '2020')
 print(population)
 
 
 x_axis = population["State"]
-y_axis = population["2040"]
+y_axis = population["2020"]
 
 plt.figure(figsize=(12,6))
 plt.bar(x_axis, y_axis, color='green')
 plt.xlabel('Top 5 States')
-plt.ylabel('Population')
-plt.title('Projected State Population by 2040')
+plt.ylabel('Population in Tens of Millions')
+plt.title('Top 5 State Populations in 2020')
 plt.show()
 
 
