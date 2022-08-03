3c70b24a6a3ee32e8680863635236329ea92b188
Justin Herfel <justinherfel@yahoo.com>
2022-08-02 22:11:47 -0400

    added data scraped from wikipedia

--- justin_herfel_data_analytics_1.py
+++ justin_herfel_data_analytics_1.py
@@ -11,21 +11,28 @@ df['2020'] = df['2020'].astype('int')
 population = df.nlargest(5, '2020')
 print(population)
 
+population["2020"] = population["2020"]/1000000
 
-x_axis = population["State"]
-y_axis = population["2020"]
+
+x_axis2 = population["State"]
+y_axis2 = population["2020"]
+
+
+website_list = pd.read_html('https://en.wikipedia.org/wiki/1920_United_States_census')
+df_1920 = website_list[2]
+print(df_1920.dtypes)
 
 # Layout 2x1
 plt.figure(figsize=(12, 8))
 
 plt.subplot(211)
-plt.bar(x_axis, y_axis, color='green')
-plt.xlabel('Top 5 States')
-plt.ylabel('Population in Tens of Millions')
-plt.title('Top 5 State Populations in 2020')
+# plt.bar(x_axis, y_axis, color='green')
+# plt.xlabel('Top 5 States')
+# plt.ylabel('Population in Millions')
+# plt.title('Top 5 State Populations in 2020')
 
 plt.subplot(212)
-plt.bar(x_axis, y_axis, color='green')
+plt.bar(x_axis2, y_axis2, color='green')
 plt.xlabel('Top 5 States')
 plt.ylabel('Population in Tens of Millions')
 plt.title('Top 5 State Populations in 2020')
