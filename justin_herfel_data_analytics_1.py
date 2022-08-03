924bdfbdc0d6d281801cb18de6af30b4b58b69fa
Justin Herfel <justinherfel@yahoo.com>
2022-08-02 22:39:13 -0400

    small changes to .py file

--- justin_herfel_data_analytics_1.py
+++ justin_herfel_data_analytics_1.py
@@ -3,22 +3,23 @@ import pandas as pd
 import os
 
 df = pd.read_csv(os.path.join(
-  os.path.dirname(__file__), 'assets', 'NationalProjections_ProjectedTotalPopulation_2020_2040_Updated12_2018.csv'))
+    os.path.dirname(__file__), 'assets', 'NationalProjections_ProjectedTotalPopulation_2020_2040_Updated12_2018.csv'))
 
-website_list = pd.read_html('https://en.wikipedia.org/wiki/1920_United_States_census')
+website_list = pd.read_html(
+    'https://en.wikipedia.org/wiki/1920_United_States_census')
 df_1920_state = website_list[1]
 df_1920_pop = website_list[2]
-
-# print(df_1920_pop.dtypes)
-# print(df.dtypes)
+print(df_1920_state)
+print(df_1920_pop)
+print(df.dtypes)
 
 df['2020'] = df['2020'].str.replace(",", "")
 df['2020'] = df['2020'].astype('int')
-population = df.nlargest(5, '2020')
-print(population)
-population["2020"] = population["2020"]/1000000
-x_axis2 = population["State"]
-y_axis2 = population["2020"]
+population2 = df.nlargest(5, '2020')
+print(population2)
+population2["2020"] = population2["2020"]/1000000
+x_axis2 = population2["State"]
+y_axis2 = population2["2020"]
 
 # Layout 2x1
 plt.figure(figsize=(12, 8))
@@ -36,8 +37,3 @@ plt.ylabel('Population in Millions')
 plt.title('Top 5 State Populations in 2020')
 
 plt.show()
-
-
-
-
-
