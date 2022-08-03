142c9e2610efade9b19e3ff07b8c5b9f82592b1b
Justin Herfel <justinherfel@yahoo.com>
2022-08-03 15:30:00 -0400

    function create for DRY purposes. Also plotted scraped web data into bar chart

--- justin_herfel_data_analytics_1.py
+++ justin_herfel_data_analytics_1.py
@@ -2,38 +2,62 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 
+def data_cleanup(df):
+    for column in df:
+        if column != "State":
+            # print(df[column])
+            df[column] = df[column].str.replace(",", "")
+            df[column] = df[column].astype('int')
+    return df
+
+def data_massage(df, column):
+    df = df.nlargest(5, column)
+    # Maybe create a separate function to go through and compare 1920 state data to 2020 state data
+    # 2020/1920 = possible or google percentage change
+    df[column] = df[column]/1000000
+    return df
+
 df = pd.read_csv(os.path.join(
     os.path.dirname(__file__), 'assets', 'NationalProjections_ProjectedTotalPopulation_2020_2040_Updated12_2018.csv'))
 
 website_list = pd.read_html(
     'https://en.wikipedia.org/wiki/1920_United_States_census')
-df_1920_state = website_list[1]
 df_1920_pop = website_list[2]
-print(df_1920_state)
+df_1920_pop = data_massage(df_1920_pop, "Population as of1920 census")
 print(df_1920_pop)
-print(df.dtypes)
+x_axis1 = df_1920_pop["State"]
+y_axis1 = df_1920_pop["Population as of1920 census"]
+# print(df.dtypes)
 
-df['2020'] = df['2020'].str.replace(",", "")
-df['2020'] = df['2020'].astype('int')
-population2 = df.nlargest(5, '2020')
-print(population2)
-population2["2020"] = population2["2020"]/1000000
+df = data_cleanup(df)
+
+population2 = data_massage(df, "2020")
+# print(population2)
 x_axis2 = population2["State"]
 y_axis2 = population2["2020"]
+population_2040 = data_massage(df, '2040')
+x_axis3 = population2["State"]
+y_axis3 = population2["2040"]
 
 # Layout 2x1
 plt.figure(figsize=(12, 8))
 
 plt.subplot(211)
-# plt.bar(x_axis, y_axis, color='green')
-# plt.xlabel('Top 5 States')
-# plt.ylabel('Population in Millions')
-# plt.title('Top 5 State Populations in 1920')
+plt.bar(x_axis1, y_axis1, color='green')
+plt.xlabel('Top 5 States')
+plt.ylabel('Population in Millions')
+plt.title('Top 5 State Populations in 1920')
 
 plt.subplot(212)
 plt.bar(x_axis2, y_axis2, color='green')
 plt.xlabel('Top 5 States')
 plt.ylabel('Population in Millions')
 plt.title('Top 5 State Populations in 2020')
+
+# plt.subplot(221)
+# plt.bar(x_axis3, y_axis3, color='green')
+# plt.xlabel('Top 5 States')
+# plt.ylabel('Population in Millions')
+# plt.title('Top 5 State Populations in 2040')
 
 plt.show()
