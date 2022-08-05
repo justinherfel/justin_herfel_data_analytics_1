89ec3cbd682eec8991c01b80fa93844d6c0171bc
Justin Herfel <justinherfel@yahoo.com>
2022-08-04 20:13:40 -0400

    total of 4 graphs, with final graph including percent change from 1920 to 2020 top 5 states

--- justin_herfel_data_analytics_1.py
+++ justin_herfel_data_analytics_1.py
@@ -5,24 +5,31 @@ import pandas as pd
 
 def data_cleanup(df):
     for column in df:
-        if column != "State":
-            # print(df[column])
+        if column != "State" and df[column].dtype != 'int64':
             df[column] = df[column].str.replace(",", "")
             df[column] = df[column].astype('int')
     return df
 
-
-def data_massage(df, column):
-    df = df.nlargest(5, column)
-    df[column] = df[column]/1000000
+def data_massage(df):
+    for column in df:
+        if  df[column].dtype == 'int64':
+            df[column] = df[column]/1000000
     return df
 
-# Maybe create a separate function to go through and compare 1920 state data to 2020 state data
-# 2020/1920 = possible or google percentage change
-# Potential new formula for finding percent increase between 2020 & 1920
-def percent_change(df, column):
-    df[column] = df[column]
-    pass
+def percent_change(df):
+
+    """Potential new formula for finding percent increase between 2020 & 1920
+    :param df: dataframe to calculate percantage change of data"""
+
+    for i, row in df.iterrows():
+        percentage_change = 0
+        if row["2020"] > row["1920"]:
+            percentage_change = ((row["2020"]-row["1920"])/row["1920"])*100
+        elif row["2020"] < row["1920"]:
+            percentage_change = -(((row["1920"]-row["2020"])/row["1920"])*100) 
+        df.at[i,'percent_change'] = percentage_change
+    return df
+
 
 df = pd.read_csv(os.path.join(
     os.path.dirname(__file__), 'assets', 'NationalProjections_ProjectedTotalPopulation_2020_2040_Updated12_2018.csv'))
@@ -30,41 +37,84 @@ df = pd.read_csv(os.path.join(
 website_list = pd.read_html(
     'https://en.wikipedia.org/wiki/1920_United_States_census')
 df_1920_pop = website_list[2]
-df_1920_pop = data_massage(df_1920_pop, "Population as of1920 census")
-print(df_1920_pop)
-x_axis1 = df_1920_pop["State"]
-y_axis1 = df_1920_pop["Population as of1920 census"]
+df_1920_pop = df_1920_pop.sort_values(by=['State'], ignore_index = True)
+# df_1920_pop = data_massage(df_1920_pop, "Population as of1920 census")
+# print(df_1920_pop)
+# x_axis1 = df_1920_pop["State"]
+# y_axis1 = df_1920_pop["Population as of1920 census"]
 # print(df.dtypes)
 
-df = data_cleanup(df)
+# df = data_cleanup(df)
 
-population2 = data_massage(df, "2020")
-# print(population2)
-x_axis2 = population2["State"]
-y_axis2 = population2["2020"]
-population_2040 = data_massage(df, '2040')
-x_axis3 = population2["State"]
-y_axis3 = population2["2040"]
+# population2 = data_massage(df, "2020")
+# # print(population2)
+# x_axis2 = population2["State"]
+# y_axis2 = population2["2020"]
+# population_2040 = data_massage(df, '2040')
+# x_axis3 = population2["State"]
+# y_axis3 = population2["2040"]
+
+# initialize data of lists.
+data = {'State': df["State"],
+        '1920': df_1920_pop["Population as of1920 census"], 
+        '2020': df["2020"],
+        '2040': df["2040"]}
+
+# Create DataFrame
+my_data = pd.DataFrame(data)
+my_data = data_cleanup(my_data) 
+my_data["percent_change"] = " "
+my_data = percent_change(my_data)
+my_data = data_massage(my_data)
+# Print the output.
+print(my_data)
+
+my_data["percent_change"] = my_data["percent_change"].astype('int')
+
+data_1920 = my_data.nlargest(5, '1920')
+data_2020 = my_data.nlargest(5, '2020')
+data_2040 = my_data.nlargest(5, '2040')
+data_percent_change = my_data.nlargest(5, 'percent_change')
+
+x_axis1 = data_1920["State"]
+y_axis1 = data_1920["1920"]
+
+x_axis2 = data_2020["State"]
+y_axis2 = data_2020["2020"]
+
+x_axis3 = data_2040["State"]
+y_axis3 = data_2040["2040"]
+
+x_axis4 = data_percent_change["State"]
+y_axis4 = data_percent_change["percent_change"]
+
 
 # Layout 2x1
 plt.figure(figsize=(12, 8))
 
-plt.subplot(311)
+plt.subplot(411)
 plt.bar(x_axis1, y_axis1, color='blue')
 plt.xlabel('Top 5 States')
 plt.ylabel('Population in Millions')
 plt.title('Top 5 State Populations in 1920')
 
-plt.subplot(312)
+plt.subplot(412)
 plt.bar(x_axis2, y_axis2, color='green')
 plt.xlabel('Top 5 States')
 plt.ylabel('Population in Millions')
 plt.title('Top 5 State Populations in 2020')
 
-plt.subplot(313)
+plt.subplot(413)
 plt.bar(x_axis3, y_axis3, color='purple')
 plt.xlabel('Top 5 States')
 plt.ylabel('Population in Millions')
-plt.title('Top 5 State Populations in 2040')
+plt.title('Top 5 State Populations in 2040 (Projected)')
 
+plt.subplot(414)
+plt.bar(x_axis4, y_axis4, color='red')
+plt.xlabel('Top 5 States')
+plt.ylabel('Percentage Change')
+plt.title('Top 5 Change in Percentage from 1920 to 2020')
+
+plt.tight_layout()
 plt.show()
